8684c758834d2a4f30ed723a49c49fcc936a28a5
Felix <t0ken97c@gmail.com>
2022-04-07 08:23:09 -0400

    day 2

--- src/components/practicemode/index.jsx
+++ src/components/practicemode/index.jsx
@@ -5,35 +5,10 @@ export default function PracticeMode (){
 
     const referenceText = useRef()
     const userInput = useRef()
-    let textIndex = 0
-    let maxIncrease = 1;
+    let textIndex = 0    
     let wordCount = 0
-
-
-
-    // function format (event) {
-        
-    //     const text = referenceText.current.textContent.split('')
-
-        
-    //     if (event.target.value[textIndex] === text[textIndex]){
-            
-    //         maxIncrease = maxIncrease + 1
-
-    //         textIndex = textIndex +1
-
-    //         userInput.current.setAttribute('maxLength', maxIncrease)
-            
-    //         console.log(event.target.value[textIndex], text[textIndex])
-
-    //     } else {
-
-    //         maxIncrease = maxIncrease - 1;
-    //         userInput.current.setAttribute('maxLength', maxIncrease)
-    //     }
-
-        
-    // }
+    let maxIncrease = 1;
+    let wordDisplay = []
 
     function format (event){
 
@@ -41,34 +16,63 @@ export default function PracticeMode (){
 
         const letters = word[wordCount].split('').map(x => x.charCodeAt())
 
-        if (word.length === wordCount) {
-
-            console.log('You Win 1')
-
-        } else if (letters.length === textIndex){
+        if (event.charCode === letters[textIndex] && event.charCode !== 32) {
             
+            console.log(event.charCode, letters[textIndex])
+            textIndex = textIndex + 1
+            maxIncrease = maxIncrease + 1
+            userInput.current.setAttribute('maxLength',maxIncrease)
+            
+        }
+        if (letters.length === textIndex){
+            
+            userInput.current.setAttribute('placeholder',wordDisplay.push(word[wordCount]))
             wordCount = wordCount + 1
-            textIndex = 0
-            console.log(word.length,wordCount)
+            maxIncrease = maxIncrease + 1
+            textIndex = 0           
+            console.log('switch')
+            console.log(wordDisplay)
 
-         } else {
+        }
+        if (wordCount === word.length){
+            
+            console.log('win')
+            userInput.current.setAttribute('disabled',true)
 
-            if (event.charCode >= 32 && event.charCode === letters[textIndex]){
+        }
+        if (event.charCode === 32){
+            console.log('space')
+        }
 
-                console.log(letters[textIndex], event.charCode)
-                textIndex = textIndex + 1   
-                maxIncrease = maxIncrease + 1   
-                userInput.current.setAttribute('maxLength',maxIncrease)
 
-            }else{
+        // if (word.length === (wordCount)) {
 
-                console.log('wrong')
-                userInput.current.setAttribute('maxLength',maxIncrease)    
+        //     console.log('You Win 1')
 
-            }
+        // } else if (letters.length === textIndex){
+            
+        //     wordCount = wordCount + 1
+        //     textIndex = 0
+        //     console.log(word.length,wordCount)
+
+        //  } else {
+
+        //     if (event.charCode >= 32 && event.charCode === letters[textIndex]){
+
+        //         console.log(letters[textIndex], event.charCode)
+        //         textIndex = textIndex + 1   
+        //         maxIncrease = maxIncrease + 1   
+        //         userInput.current.setAttribute('maxLength',maxIncrease)
+
+        //     }else{
+
+        //         console.log('wrong')
+        //         userInput.current.setAttribute('maxLength',maxIncrease)    
+
+        //     }
 
         
-        }  
+        // }  
 
     }
 

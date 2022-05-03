07c49478b767ace048c03415c22386cedb0ea168
Felix <t0ken97c@gmail.com>
2022-05-02 21:28:15 -0400

    bug report page finished

--- src/components/bugreport/index.jsx
+++ src/components/bugreport/index.jsx
@@ -10,15 +10,19 @@ export default function BugReport () {
         e.preventDefault();
 
         emailjs.sendForm(
-            'service_cisq9mt',
-            'template_ndc45qq',
+            'service_hhpbevl',
+            'template_m5quqir',
             form.current,
-            'qQWg7UXA_Y_UeDt4g'
+            'uE9ktrV1sT1SDdPNZ'
         ).then((result) => {
             window.alert('success')
+            form.current.reset()
+            console.log(form.current)
         }, (error) => {
             window.alert('fail')
         })
+
+        
     }
 
     return (
@@ -28,6 +32,13 @@ export default function BugReport () {
             <h1>
                 Bug Report
             </h1>
+
+            <p>
+                I am not the greatest developer, thus this form below is for you to 
+                report any bugs you many have found. Please be as thorough as possible, 
+                and if you're up to it include the steps to recreate the bug, as this would
+                make it easier to find the bug and would be very appreciated. Thank you !
+            </p>
 
             <div className='contact-form'>
                 <form ref={form} onSubmit={sendEmail}>
@@ -41,7 +52,7 @@ export default function BugReport () {
                             <input type="email" name="email" placeholder='Email' required/>
                         </li>
                         <li>
-                            <input type="text" name="subject" placeholder='Subject' required/>
+                            <input type="text" name='subject' placeholder='Subject' />
                         </li>
                         <li>
                             <textarea placeholder='Message' name='message' resize='none' required></textarea>

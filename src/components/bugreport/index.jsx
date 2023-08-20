3a7991cc36a3130cc3c04719f4a9e002fd6a126d
Fpantoja2001 <121701118+Fpantoja2001@users.noreply.github.com>
2023-08-20 18:26:09 -0400

    feat: known bugs added + bug report page changes

--- src/components/bugreport/index.jsx
+++ src/components/bugreport/index.jsx
@@ -28,39 +28,77 @@ export default function BugReport () {
     return (
 
         <div className='wrapperBugReport'>
-
-            <h1>
-                Bug Report
-            </h1>
-
-            <p>
-                this form is for you to report any bugs you many have found. Please be as thorough as possible, Thank you!
-            </p>
-
-            <div className='contact-form'>
-                <form ref={form} onSubmit={sendEmail}>
-
-                    <ul>
-                        <li className='left'>
-                            <input type="text" name="name" placeholder='Name' required/>
-                        </li>
-
-                        <li className='left'>
-                            <input type="email" name="email" placeholder='Email' required/>
-                        </li>
-                        <li>
-                            <input type="text" name='subject' placeholder='Subject' />
-                        </li>
-                        <li>
-                            <textarea placeholder='Message' name='message' resize='none' required></textarea>
-                        </li>
-                        <li>
-                            <input type="submit" className='submit-button'  value='Send'/>
-                        </li>
-                    </ul>
-                </form>
-
+            <div className='containerKnownBugs'>
+                <h1>
+                    Known Bugs
+                </h1>
+                <div className='textHolder'>
+                    <div>
+                       b1.1 - 
+                    </div>
+                    <p>
+                        Quote length selection dissapears on next game button press and reappears on game start press.
+                    </p>
+                    
+                </div>
+                <div  className='textHolder'>
+                    <div>
+                       b1.2 - 
+                    </div>
+                    <p>
+                        The inability to enter in double dash '--' when a quote requires it.  
+                    </p>
+                    
+                </div>
+                <div  className='textHolder'>
+                    <div>
+                       b1.3 - 
+                    </div>
+                    <p>
+                        Each indivisual period in '...' will not turn green or red if it is either correct or wrong 
+                        until all characters in its spot have been filled.
+                    </p>
+                    
+                </div>
             </div>
+
+            <div className='containerBugReport'>
+                
+                <h1>
+                    Bug Report
+                </h1>
+
+                <p>
+                    This form is for you to report any bugs you many have found. Please be as thorough as possible, Thank you!
+                </p>
+
+                <div className='contact-form'>
+                    <form ref={form} onSubmit={sendEmail}>
+
+                        <ul>
+                            <li className='left'>
+                                <input type="text" name="name" placeholder='Name' required/>
+                            </li>
+
+                            <li className='left'>
+                                <input type="email" name="email" placeholder='Email' required/>
+                            </li>
+                            <li>
+                                <input type="text" name='subject' placeholder='Subject' />
+                            </li>
+                            <li>
+                                <textarea placeholder='Message' name='message' resize='none' required></textarea>
+                            </li>
+                            <li>
+                                <input type="submit" className='submit-button'  value='Send'/>
+                            </li>
+                        </ul>
+                    </form>
+
+                </div>
+            </div>
+
+            
 
         </div>
         

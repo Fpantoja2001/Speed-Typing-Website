4694683b9bef24d7693827e30f54ecf3970e5d48
Fpantoja2001 <121701118+Fpantoja2001@users.noreply.github.com>
2023-08-22 11:21:46 -0400

    fix: unsupported devices

--- src/index.jsx
+++ src/index.jsx
@@ -9,23 +9,37 @@ import ReleaseNotes from './components/releasenotes';
 // import Landing from './components/landing';
 // import PracticeMode from './components/landing';
 import Landing from './components/landing';
+import {isMobile} from 'react-device-detect';
 
 function App() {
-  return (
-    <div className='app'>
-      <div className='sections'>
-        <Navbar/>
-        
-        <Routes>
-          <Route path='/' element={<Landing/>}/>
-          <Route path='/releaseNotes' element={<ReleaseNotes/>}/>
-          <Route path='/bugReport' element={<BugReport/>}/>
-        </Routes>
+  console.log(isMobile)
 
-        <Footer/>
+  if (isMobile) {
+    return(
+      <div className='mobileApp'>
+        <h1>This game has unfortunately not been optimized for mobile yet. Open this on your desktop to access the website. <br /><br />08/22/2023</h1>
+
       </div>
-    </div>
-  );
+      
+    )
+  } else {
+    return (
+      <div className='app'>
+        <div className='sections'>
+          <Navbar/>
+          
+          <Routes>
+            <Route path='/' element={<Landing/>}/>
+            <Route path='/releaseNotes' element={<ReleaseNotes/>}/>
+            <Route path='/bugReport' element={<BugReport/>}/>
+          </Routes>
+
+          <Footer/>
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(<Router><App/></Router>)
